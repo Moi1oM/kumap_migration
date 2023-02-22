e899f8505f253c87900a8831e06f7515dafa81cb
JHLvJz <jhsu1619@gmail.com>
2023-02-22 18:18:59 +0900

    Entrance BE 연동

--- migra-kumap/pages/entrance/[id].tsx
+++ migra-kumap/pages/entrance/[id].tsx
@@ -17,50 +17,67 @@ import {
 } from "../constants/entranceAtom";
 import { useRecoilState } from "recoil";
 import EntranceModal from "../components/entrance/EntranceModal";
+import {
+  clickedBuildingState,
+  modalBuildingNameState,
+} from "../constants/atom";
 
 const Post = () => {
   const [map, setMap] = useState<any>(
     /** @type google.mpas.GoogleMap */ //indexMapState
     null
   );
-  const [name, setName] = useState();
-  const [pk, setPk] = useState<any>();
-  const [lat, setLat] = useState();
-  const [lon, setLon] = useState();
-  const [entrances, setEntrances] = useState<any[]>();
-  const [schoolj, setSchoolj] = useState();
+  const [name, setName] = useState(); //클릭된 빌딩 이름
+  const [pk, setPk] = useState<any>(); //클릭된 빌딩 pk
+  const [clickedBuildilng, setClickedBuilding] =
+    useRecoilState(clickedBuildingState);
+
+  const [lat, setLat] = useState(); // 클릭된 빌딩 위도
+  const [lon, setLon] = useState(); // 클릭된 빌딩 경도
+  const [entrances, setEntrances] = useState<any[]>(); // 클릭된 빌딩에 속하는 출입구 객체들
   const [mapCenter, setMapCenter] = useState();
   const [isMarkerLoaded, setIsMarkerLoaded] = useState(0);
   const [todayHour, setTodayHour] = useState(0);
   const [todayMin, setTodayMin] = useState(0);
-  const [entranceName, setEntranceName] = useRecoilState(entranceNameState);
-  const [entranceTime, setEntranceTime] = useRecoilState(entranceTimeState);
+  const [entranceName, setEntranceName] = useRecoilState(entranceNameState); // 클릭된 출입구 이름
+  const [entranceTime, setEntranceTime] = useRecoilState(entranceTimeState); // 클릭된 출입구 시간
 
-  const [mapLat, setMapLat] = useState("");
-  const [mapLon, setMapLon] = useState("");
+  const [mapLat, setMapLat] = useState(""); // 출입구 위도
+  const [mapLon, setMapLon] = useState(""); // 출입구 경도
 
-  const [entranceModal, setEntranceModal] = useRecoilState(entranceModalState);
-
+  const [entranceModal, setEntranceModal] = useRecoilState(entranceModalState); // 출입구 클릭 때의 모달
+  const [buildingname, buildingSetName] = useRecoilState(
+    modalBuildingNameState
+  ); // 클릭된 빌딩에 이름
+  console.log(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/v1/entrances/${buildingname}`
+  );
   const router = useRouter();
   const { id } = router.query;
-  const dataFetch = async () => {
+
+  // const dataFetch1 = async () => {
+  //   await axios.get();
+  // };
+
+  const dataFetch2 = async () => {
     await axios
-      .get(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/v1/entrances/${pk}`)
+      .get(
+        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/v1/entrances/all?building=${buildingname}`
+      )
       .then(function (res) {
-        // console.log(res);
         const entrancesData = res.data;
-        console.log("entrances data", entrancesData);
+        console.log("entrancesData", entrancesData); //출입구 객체 배열
         entrancesData.map((entrance: any) => {
           console.log(
-            `entrance/${entrance.entrance_name.substr(0, 1)}.png`,
-            entrance.pk
+            `entrance/${entrance.entrance_loc.substr(0, 1)}.png`,
+            entrance.id
           );
         });
-        setName(res.data.building_name_ko);
-        setLat(res.data.latitude);
-        setLon(res.data.longitude);
-        setEntrances(JSON.parse(res.data.entrances));
-        setSchoolj(res.data.schoolj);
+        console.log("-------", clickedBuildilng);
+        setName(clickedBuildilng!["building_name_ko"]);
+        setLat(clickedBuildilng!["latitude"]); // 클릭된 건물 위도
+        setLon(clickedBuildilng!["longitude"]); // 클릭된 건물 경도
+        setEntrances(entrancesData); //개별 출입구가 아니라 빌딩에 해당하는 출입구들이구나
       })
       .catch((err) => {
         console.log("에러", err);
@@ -69,7 +86,8 @@ const Post = () => {
   useEffect(() => {
     setPk(id);
     if (pk) {
-      dataFetch();
+      // dataFetch1();
+      dataFetch2();
     }
   }, [id, pk]);
   const todayTime = () => {
@@ -103,12 +121,12 @@ const Post = () => {
   }
   const markerClicked = (ent: any) => {
     console.log(ent);
-    setMapLat(ent.fields.entrance_lat);
-    setMapLon(ent.fields.entrance_lon);
-    setEntranceName(ent.fields.entrance_name);
-    setEntranceTime(ent.fields.entrance_time);
+    setMapLat(ent.latitude);
+    setMapLon(ent.longtitude);
+    setEntranceName(ent.entrance_loc);
+    setEntranceTime(ent.time);
     setEntranceModal(true);
-    map.panTo({ lat: mapLat, lng: mapLon });
+    map.panTo({ lat: Number(mapLat), lng: Number(mapLon) });
   };
   // console.log(name);
   // console.log(entrances);
@@ -117,17 +135,13 @@ const Post = () => {
     router.push("/index");
   };
 
-  // useEffect(() => {
-  //   map.panTo({ lat: mapLat, lng: mapLon });
-  // }, [mapLat, mapLon]);
-
   return (
     <div>
       <GoogleMap
         id="65e8ebc6cdf29142"
         options={mapOptions}
         zoom={19}
-        center={{ lat: lat!, lng: lon! }}
+        center={{ lat: Number(lat!), lng: Number(lon!) }}
         mapTypeId={google.maps.MapTypeId.ROADMAP}
         mapContainerStyle={{ width: "100%", height: "100vh" }}
         onLoad={(indexMap) => {
@@ -136,15 +150,12 @@ const Post = () => {
         }}
       >
         {entrances?.map((entrance) => (
-          <MarkerContainer key={entrance.pk}>
+          <MarkerContainer key={entrance.id}>
             <Marker
-              icon={`/entrance/${entrance.fields.entrance_name.substr(
-                0,
-                1
-              )}.png`}
+              icon={`/entrance/${entrance.entrance_loc.substr(0, 1)}.png`}
               position={{
-                lat: entrance.fields.entrance_lat,
-                lng: entrance.fields.entrance_lon,
+                lat: Number(entrance.latitude),
+                lng: Number(entrance.longitude),
               }}
               onLoad={() => console.log("Marker Loaded")}
               onClick={(e) => {
